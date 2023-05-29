3ee5cc030f33fe2ab346ada7ebf4ff753b89c7f2
fancySummer19 <2458290516@qq.com>
2023-05-29 20:25:59 +0800

    master

--- pages/vedio/vedio.js
+++ pages/vedio/vedio.js
@@ -1,4 +1,5 @@
 // pages/vedio/vedio.js
+
 import request from '../../utils/request'
 Page({
 
@@ -6,19 +7,55 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        placeholderContent: '',
+        hotList: [],
+        hotListDetail: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        //获取几个热搜列表的id
+        this.gethot()
+        //获取列表详情
+        // let id = this.hotList[0]
+        // this.gethotDetail(id)
     },
     //确定搜索时出发
-    onSearch(e){
+    onSearch(e) {
         wx.navigateTo({
-          url: '/pages/searchDetail/searchDetail?searchWord='+e.detail,
+            url: '/pages/searchDetail/searchDetail?searchWord=' + e.detail,
+        })
+    },
+    //获取几个热搜列表的id
+    async gethot() {
+        let res = await request('/toplist')
+        let hotArr = []
+        for (let i = 0; i <= 3; i++) {
+            hotArr.push(res.list[i].id)
+        }
+        this.gethotDetail(hotArr[0])
+        this.gethotDetail(hotArr[1])
+        this.gethotDetail(hotArr[2])
+        this.gethotDetail(hotArr[3])
+        this.setData({
+            hotList: hotArr
+        })
+    },
+    //获取热榜的详情
+    async gethotDetail(listId) {
+        let res = await request('/playlist/detail', {
+            id: listId
+        })
+        let hotDetail = {}
+        hotDetail.title = res.playlist.name
+        hotDetail.list=res.playlist.tracks.slice(0,20)
+        let hotListDetailTemp = []
+        hotListDetailTemp = this.data.hotListDetail
+        hotListDetailTemp.push(hotDetail)
+        this.setData({
+            hotListDetail:hotListDetailTemp
         })
     },
 
